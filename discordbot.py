a522ea72b678af3015e3833890f80d34f264c3f2
Maki-Kana <72199250+Maki-Kana@users.noreply.github.com>
2020-11-27 20:48:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,20 +3,16 @@ import os
 import traceback
 
 bot = commands.Bot(command_prefix='!')
-token = os.environ['DISCORD_BOT_TOKEN']
+token = os.environ["DISCORD_BOT_TOKEN"]
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!shorturl'):
-        embed = discord.Embed()
-        embed.set_author(name="短縮URLにしたよ", url="https://google.com")
-        embed.add_field(name="undefined", value="undefined", inline=False)
-        await self.bot.say(embed=embed)
+    global channel
+    if message.content == prefix + 'setchannel':
+        channel = message.channel
+    if channel:
+        channel.send(message.content)
+
+
 bot.run(token)
